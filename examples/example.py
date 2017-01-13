8ee5e2ac15b0d7732c37d58b24d929a22b0f602a
sgill2 <samgill@dhcp-204148.bio.uci.edu>
2017-01-12 19:33:28 -0800

    added reading periodic box vectors from amber file

--- examples/example.py
+++ examples/example.py
@@ -38,18 +38,26 @@ md_simulation.context.setVelocitiesToTemperature(temperature)
 # add reporters
 md_simulation.reporters.append(openmm.app.dcdreporter.DCDReporter('traj.dcd', 1000))
 md_simulation.reporters.append(HDF5Reporter('traj.h5', 1000))
+#enforce peroidc box from inpcrd file
+if inpcrd.boxVectors is not None:
+    md_simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+    dummy_simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+
 practice_run = SimNCMC(temperature=temperature, residueList=lig_atoms)
 # set nc attributes
 numIter = 100
 nstepsNC = 100
 nstepsMD = 1000
 #functions here defines the equation of the lambda peturbation of the sterics and electrostatics over the course of a ncmc move
-functions = { 'lambda_sterics' : '2*lambda * step(0.5 - lambda) + (1.0 - step(0.5 - lambda))', 'lambda_electrostatics' : '2*(lambda - 0.5) * step(lambda - 0.5)' }
+functions = { 'lambda_sterics' : 'step(0.2-lambda) + step(lambda-0.2)*step(0.8-lambda)*abs(lambda-0.5)*1/0.3 + step(lambda-0.8)', 
+			'lambda_electrostatics' : 'step(0.2-lambda)- 1/0.2*lambda*step(0.2-lambda) + 1/0.2*(lambda-0.8)*step(lambda-0.8)' }
 nc_integrator = NCMCVVAlchemicalIntegrator(temperature, alchemical_system, functions, nsteps=nstepsNC, direction='insert', timestep=0.001*unit.picoseconds)
 md_integrator = openmm.openmm.LangevinIntegrator(temperature, 1/unit.picosecond, 0.002*unit.picoseconds)
 nc_context = openmm.Context(alchemical_system, nc_integrator)
+if inpcrd.boxVectors is not None:
+    nc_context.setPeriodicBoxVectors(*inpcrd.boxVectors)
 #during the ncmc move, perform a rotation around the center of mass at the start of step 1
-ncmove = [[practice_run.rotationalMove, [1]]]
+ncmove = [[practice_run.rotationalMove, [50]]]
 # actually run
 practice_run.get_particle_masses(testsystem.system, residueList = lig_atoms)
 practice_run.runSim(md_simulation, nc_context, nc_integrator, dummy_simulation, movekey=ncmove, niter=numIter, nstepsNC=nstepsNC, nstepsMD=nstepsMD)
