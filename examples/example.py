126557ecc64baf7bd6f97ef9abf4c8b320af19fd
Nathan Lim <limn1@uci.edu>
2017-01-26 17:53:24 -0800

    Refactored example for clarity, added notebook for testing, added utilities script

--- examples/example.py
+++ examples/example.py
@@ -1,12 +1,10 @@
-import simtk.openmm.app as app
-import simtk.openmm as mm
-import simtk.unit as unit
-from simtk.openmm.app import Simulation
-#from openmmtools.testsystems import TestSystem
+from simtk import unit, openmm
+from simtk.openmm import app
 from alchemy import AbsoluteAlchemicalFactory, AlchemicalState
 
-from blues.ncmc import *
-from blues.ncmc_switching import *
+import blues.utils as utils
+import blues.ncmc as ncmc
+import blues.ncmc_switching as ncmc_switching
 from blues.smartdart import SmartDarting
 
 import sys
@@ -28,93 +26,86 @@ def timeIntegration(context, steps, initialSteps):
     return elapsed.seconds + elapsed.microseconds*1e-6
 
 def runNCMC(options):
-    platform = mm.Platform.getPlatformByName(options.platform)
-    ## load systems and make alchemical systems
-    coord_file = 'eqToluene.inpcrd'
-    top_file =   'eqToluene.prmtop'
-    prmtop = app.AmberPrmtopFile(top_file)
-    inpcrd = app.AmberInpcrdFile(coord_file)
-    testsystem = prmtop.createSystem(nonbondedMethod=app.PME,
-                                      nonbondedCutoff=1*unit.nanometer,
-                                      constraints=app.HBonds)
-
-    # helper function to get list of ligand atoms
-    lig_atoms = get_lig_residues(lig_resname='LIG',
-                                 coord_file=coord_file,
-                                 top_file=top_file)
-
-    # create alchemical system using alchemy functions
-    factory = AbsoluteAlchemicalFactory(testsystem,
-                                        ligand_atoms=lig_atoms,
-                                        annihilate_sterics=True,
-                                        annihilate_electrostatics=True)
-
-    alchemical_system = factory.createPerturbedSystem()
-
-    ## set up OpenMM simulations
+    # Define some constants
     temperature = 300.0*unit.kelvin
     friction = 1/unit.picosecond
     dt = 0.002*unit.picoseconds
-    # functions describes how lambda scales with nstepsNC
-    md_integrator = mm.LangevinIntegrator(temperature, friction, dt)
-    dummy_integrator = mm.LangevinIntegrator(temperature, friction, dt)
+    # set nc attributes
+    numIter = 10
+    nstepsNC = 10
+    nstepsMD = 50
+    #functions here defines the equation of the lambda peturbation of the sterics and electrostatics over the course of a ncmc move
+    functions = { 'lambda_sterics' : 'step(0.199999-lambda) + step(lambda-0.2)*step(0.8-lambda)*abs(lambda-0.5)*1/0.3 + step(lambda-0.800001)',
+                'lambda_electrostatics' : 'step(0.2-lambda)- 1/0.2*lambda*step(0.2-lambda) + 1/0.2*(lambda-0.8)*step(lambda-0.8)' }
+    # Initialize integrators
+    md_integrator = openmm.LangevinIntegrator(temperature, friction, dt)
+    alch_integrator = openmm.LangevinIntegrator(temperature, friction, dt)
+    # Define platform
+    platform = openmm.Platform.getPlatformByName(options.platform)
 
-    md_sim = Simulation(topology=prmtop.topology,
-                        system=testsystem,
-                        integrator=md_integrator,
-                        platform=platform)
+    # Obtain topologies/positions
+    prmtop = app.AmberPrmtopFile('eqToluene.prmtop')
+    inpcrd = app.AmberInpcrdFile('eqToluene.inpcrd')
 
-    # dummy_simulation is used to perform alchemical corrections and serves as a reporter for ncmc moves
-    dummy_sim = Simulation(topology=prmtop.topology,
-                           system=testsystem,
-                           integrator=dummy_integrator,
-                           platform=platform)
+    # Generate OpenMM System
+    system = prmtop.createSystem(nonbondedMethod=app.PME,
+                                nonbondedCutoff=1*unit.nanometer,
+                                constraints=app.HBonds)
 
-
+    # Initailize MD Simulation
+    md_sim = app.Simulation(prmtop.topology, system,
+                            md_integrator, platform)
     md_sim.context.setPositions(inpcrd.positions)
     md_sim.context.setVelocitiesToTemperature(temperature)
-    if inpcrd.boxVectors is not None:
-        md_sim.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
-        dummy_sim.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+    md_sim.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
 
-
-    # set nc attributes
-    numIter = 50
-    nstepsNC = 100
-    nstepsMD = 1000
-
-    # add reporters
+    # Add reporters for MD simulation
     md_sim.reporters.append(app.dcdreporter.DCDReporter('traj.dcd', nstepsMD))
     md_sim.reporters.append(HDF5Reporter('traj.h5', nstepsMD))
-    practice_run = SimNCMC(temperature=temperature, residueList=lig_atoms)
 
-    #functions here defines the equation of the lambda peturbation of the sterics and electrostatics over the course of a ncmc move
-    functions = { 'lambda_sterics' : 'step(0.199999-lambda) + step(lambda-0.2)*step(0.8-lambda)*abs(lambda-0.5)*1/0.3 + step(lambda-0.800001)', 
-                'lambda_electrostatics' : 'step(0.2-lambda)- 1/0.2*lambda*step(0.2-lambda) + 1/0.2*(lambda-0.8)*step(lambda-0.8)' }
-    nc_integrator = NCMCVVAlchemicalIntegrator(temperature,
-                                               alchemical_system,
+    # Get ligand atom indices
+    ligand_atoms = utils.atomIndexfromTop('LIG', prmtop.topology)
+
+    #Initialize Alchemical Simulation
+    # performs alchemical corrections
+    # Reporter for NCMC moves
+    alch_sim = app.Simulation(prmtop.topology, system,
+                              alch_integrator, platform)
+    alch_sim.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+    # Generate Alchemical System
+    factory = AbsoluteAlchemicalFactory(system, ligand_atoms,
+                                        annihilate_sterics=True,
+                                        annihilate_electrostatics=True)
+    alch_system = factory.createPerturbedSystem()
+
+    # Generate NC Integrator/Contexts
+    nc_integrator = ncmc_switching.NCMCVVAlchemicalIntegrator(temperature,
+                                               alch_system,
                                                functions,
                                                nsteps=nstepsNC,
                                                direction='insert',
                                                timestep=0.001*unit.picoseconds,
                                                steps_per_propagation=1)
 
-    nc_context = mm.Context(alchemical_system, nc_integrator, platform)
-    if inpcrd.boxVectors is not None:
-        nc_context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+    nc_context = openmm.Context(alch_system, nc_integrator, platform)
+    nc_context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+
+    # Initialize BLUES engine
+    blues_run = ncmc.SimNCMC(temperature, residueList=ligand_atoms)
 
     #during the ncmc move, perform a rotation around the center of mass at the start of step 49 (again to maintain symmetry of ncmc move
-    ncmove = [[practice_run.rotationalMove, [49]]]
+    rot_step = (nstepsNC/2) -1
+    nc_move = [[blues_run.rotationalMove, [rot_step]]]
 
     # actually run
-    practice_run.get_particle_masses(testsystem, residueList=lig_atoms)
-    practice_run.runSim(md_sim, nc_context, nc_integrator,
-                        dummy_sim, movekey=ncmove,
-                        niter=numIter, nstepsNC=nstepsNC, nstepsMD=nstepsMD,
-                        alchemical_correction=True)
+    blues_run.get_particle_masses(system, residueList=ligand_atoms)
+    blues_run.runSim(md_sim, nc_context, nc_integrator,
+                    alch_sim, movekey=nc_move,
+                    niter=numIter, nstepsNC=nstepsNC, nstepsMD=nstepsMD,
+                    alchemical_correction=True)
 
     nc_time = timeIntegration(nc_context, nstepsNC, 5)
-    md_time = timeIntegration(md_sim.context, nstepsMD, 20)
+    md_time = timeIntegration(md_sim.context, nstepsMD, 25)
 
     nc_steps = int(nstepsNC*1.0/nc_time)
     md_steps = int(nstepsMD*1.0/md_time)
@@ -122,18 +113,17 @@ def runNCMC(options):
     print('NC: Integrated %d steps in %g seconds' % (nstepsNC, nc_time))
     print('MD: Integrated %d steps in %g seconds' % (nstepsMD, md_time))
 
-    print('NC: %g ns/day' % (dt/2*nstepsNC/nc_time).value_in_unit(unit.nanoseconds))
-    print('MD: %g ns/day' % (dt*nstepsMD/nc_time).value_in_unit(unit.nanoseconds))
+    print('NC: %g ns/day' % (dt/2*nstepsNC*86400/nc_time).value_in_unit(unit.nanoseconds))
+    print('MD: %g ns/day' % (dt*nstepsMD*86400/nc_time).value_in_unit(unit.nanoseconds))
 
 parser = OptionParser()
-platformNames = [mm.Platform.getPlatform(i).getName() for i in range(mm.Platform.getNumPlatforms())]
+platformNames = [openmm.Platform.getPlatform(i).getName() for i in range(openmm.Platform.getNumPlatforms())]
 parser.add_option('--platform', dest='platform', choices=platformNames, help='name of the platform to benchmark')
 (options, args) = parser.parse_args()
 if len(args) > 0:
     parser.error('Unknown argument: '+args[0])
 if options.platform is None:
     parser.error('No platform specified')
-print('Platform:', options.platform)
-
-import cProfile, pstats, io
-cProfile.run('runNCMC(options)', '{}.profile'.format(options.platform), sort=-1)
+else:
+    print('Platform:', options.platform)
+    runNCMC(options)
