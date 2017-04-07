fixed sign error for change_angle

blues/lin_math.py
@@ -34,7 +34,7 @@ def adjust_angle(a,b, radians, maintain_magnitude=True):
     #determine how much to scale the perpendicular
     #component of a to adjust the angle
     scale = np.tan(float(radians))*mag_para/mag_perp
-    c = para + perp*scale
+    c = para - perp*scale
     #if True, adjusts vector c to maintain same
     #total magnitude as the original (a).
     if maintain_magnitude == True:
@@ -164,3 +164,51 @@ def old_old_getRotTrans(apos, bpos, residueList=None):
         b_res[index] = bpos[i]
     rot, trans, centa, centb, centroid_difference = rigid_transform_3D(a_res, b_res)
     return rot, centroid_difference
+
+def kabsch(p, q, center):
+    """
+    The optimal rotation matrix U is calculated and then used to rotate matrix
+    P unto matrix Q so the minimum root-mean-square deviation (RMSD) can be
+    calculated.
+    Using the Kabsch algorithm with two sets of paired point P and Q,
+    centered around the center-of-mass.
+    Each vector set is represented as an NxD matrix, where D is the
+    the dimension of the space.
+    The algorithm works in three steps:
+    - a translation of P and Q
+    - the computation of a covariance matrix C
+    - computation of the optimal rotation matrix U
+    http://en.wikipedia.org/wiki/Kabsch_algorithm
+    Parameters:
+    P -- (N, number of points)x(D, dimension) matrix
+    Q -- (N, number of points)x(D, dimension) matrix
+    Returns:
+    U -- Rotation matrix
+    """
+    N = p.shape[0]; # total points
+
+    P = p - np.tile(p[center], (N, 1))
+    Q = q - np.tile(q[center], (N, 1))
+
+    # Computation of the covariance matrix
+    C = np.dot(np.transpose(P), Q)
+
+    # Computation of the optimal rotation matrix
+    # This can be done using singular value decomposition (SVD)
+    # Getting the sign of the det(V)*(W) to decide
+    # whether we need to correct our rotation matrix to ensure a
+    # right-handed coordinate system.
+    # And finally calculating the optimal rotation matrix U
+    # see http://en.wikipedia.org/wiki/Kabsch_algorithm
+    V, S, W = np.linalg.svd(C)
+    d = (np.linalg.det(V) * np.linalg.det(W)) < 0.0
+
+    if d:
+        S[-1] = -S[-1]
+        V[:, -1] = -V[:, -1]
+
+    # Create Rotation matrix U
+    U = np.dot(V, W)
+
+    return U
+

blues/moldart.py
@@ -6,7 +6,7 @@ import simtk.unit as unit
 import numpy as np
 from blues.ncmc import SimNCMC, get_lig_residues
 import mdtraj as md
-from blues.lin_math import calc_rotation_matrix, adjust_angle
+from blues.lin_math import calc_rotation_matrix, adjust_angle, kabsch
 from blues.lin_math import getRotTrans
 import itertools
 import chemcoord as cc
@@ -477,6 +477,9 @@ class MolDart(SimNCMC):
             dart_three[i] = binding_mode_pos[rand_index].xyz[0][residueList[self.buildlist[i, 0]]]
             dart_ref[i] = binding_mode_pos[rand_index].xyz[0][residueList[self.buildlist[i, 0]]]
             print('dart3 1', dart_three)
+        print('debugging')
+        print('before sim', sim_three)
+        print('before ref', ref_three)
         change_three = np.copy(sim_three)
         vec1_sim = sim_three[vector_list[0][0]] - sim_three[vector_list[0][1]]
         vec2_sim = sim_three[vector_list[1][0]] - sim_three[vector_list[1][1]]
@@ -502,21 +505,30 @@ class MolDart(SimNCMC):
         ad_vec = ad_vec / np.linalg.norm(ad_vec) * self.internal_zmat[binding_mode_index].frame['bond'][self.buildlist[1,0]]/10.
         print('ad_vec length', np.linalg.norm(ad_vec))
         #apply changed vector to center coordinate to get new position of first particle
+        print('change before before rot', change_three)
+
         nvec2_sim = vec2_sim / np.linalg.norm(vec2_sim) * self.internal_zmat[binding_mode_index].frame['bond'][self.buildlist[2,0]]/10.
         print('new angle1', np.degrees(calc_angle(ad_vec, nvec2_sim)))
         change_three[vector_list[0][0]] = sim_three[vector_list[0][1]] + ad_vec
         change_three[vector_list[1][0]] = sim_three[vector_list[0][1]] + nvec2_sim
+        print('change before rot', change_three)
         rot_mat, centroid = getRotTrans(change_three, ref_three, center=vector_list[0][1])
+        print('ref_three', ref_three)
+        print('sim_three', sim_three)
+        print('change_three', change_three)
         print('centroid movement', centroid, np.linalg.norm(centroid))
         #TODO CONTINUE FROM HERE
         #perform the same angle change on new coordinate
-        centroid_orig = np.mean(dart_three, axis=0)
+#        centroid_orig = np.mean(dart_three, axis=0)
+        centroid_orig = dart_three[vector_list[0][1]]
         #perform rotation
         dist1 = np.linalg.norm(dart_three[1] - dart_three[0])
         dist2 = np.linalg.norm(dart_three[2] - dart_three[0])
-
-#        dart_three = (dart_three -  np.tile(centroid_orig, (3,1))).dot(rot_mat) + np.tile(centroid_orig, (3,1)) - np.tile(centroid, (3,1))
-        dart_three = dart_three  - np.tile(centroid, (3,1))
+        print('rot_mat', rot_mat)
+        other_rot = kabsch(change_three, ref_three, vector_list[0][1]) 
+        print('other_rot', other_rot)
+        dart_three = (dart_three -  np.tile(centroid_orig, (3,1))).dot(rot_mat) + np.tile(centroid_orig, (3,1)) - np.tile(centroid, (3,1))
+#        dart_three = dart_three  - np.tile(centroid, (3,1))
 
         vec1_dart = dart_three[vector_list[0][0]] - dart_three[vector_list[0][1]]
         vec2_dart = dart_three[vector_list[1][0]] - dart_three[vector_list[1][1]]