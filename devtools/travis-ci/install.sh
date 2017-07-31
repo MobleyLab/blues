3050af53e3b1167cb8c255e0e5645ce3a63a6a77
sgill2 <samgill@dhcp-204148.bio.uci.edu>
2017-07-31 13:12:26 -0700

    revert merge changes

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -16,8 +16,10 @@ bash $MINICONDA -b -p $MINICONDA_HOME
 # Configure miniconda
 export PIP_ARGS="-U"
 export PATH=$MINICONDA_HOME/bin:$PATH
-conda config --add channels conda-forge
-conda install --yes conda\>=4.3 conda-build jinja2 anaconda-client pip
-
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update --yes conda
+conda install --yes conda-build jinja2 anaconda-client pip
+conda info -a
 # Restore original directory
 popd
