Fixed mp180 range and fixed proposed for CCW rotation

blues/moves.py
@@ -523,7 +523,7 @@ class SideChainMove(Move):
         # Classify starting angle
         if -1.3 <= dihedralangle <= -0.9:
             current_rot = 'm60'
-        elif -2.94159 <= dihedralangle <= -3.14159:
+        elif -3.14159 <= dihedralangle <= -2.94159:
             current_rot = 'mp180'
         elif 0.9 <= dihedralangle <= 1.3:
             current_rot = 'p60'
@@ -537,12 +537,12 @@ class SideChainMove(Move):
         my_theta, my_target_atoms, my_res, my_bond = self.chooseBondandTheta()
         moveOK = False
 
-        proposed = (postrelax_dihedralangle + my_theta + math.pi)%(2*math.pi)-math.pi
+        proposed = (postrelax_dihedralangle - my_theta + math.pi)%(2*math.pi)-math.pi
 
         while moveOK == False:
             if -1.3 <= proposed <= -0.9 and current_rot != 'm60':
                 moveOK = True
-            elif -2.94159 <= proposed <= -3.14159 and current_rot != 'mp180':
+            elif -3.14159 <= proposed <= -2.94159 and current_rot != 'mp180':
                 moveOK = True
             elif 0.9 <= proposed <= 1.3 and current_rot != 'p60':
                 moveOK = True
@@ -551,7 +551,7 @@ class SideChainMove(Move):
             else:
                 if verbose: print("Proposed theta rejected",my_theta)
                 my_theta, my_target_atoms, my_res, my_bond = self.chooseBondandTheta()
-                proposed = (postrelax_dihedralangle + my_theta + math.pi)%(2*math.pi)-math.pi
+                proposed = (postrelax_dihedralangle - my_theta + math.pi)%(2*math.pi)-math.pi
                 moveOK = False
 
         print('This is the new proposed dihedral',proposed)
@@ -626,17 +626,21 @@ class SideChainMove(Move):
         post_pos = context.getState(getPositions=True).getPositions(asNumpy=True)
         indices = np.asarray([[0,4,6,8]])
         angle = self.getDihedral(post_pos,indices)
-
+        print("This is the new angle:",angle)
         if -1.3 <= angle <= -0.9:
             bin = True
-        elif -2.94159 <= angle <= -3.14159:
+            print("final bin ok")
+        elif -3.14159 <= angle <= -2.94159:
             bin = True
         elif 0.9 <= angle <= 1.3:
             bin = True
+            print("final bin ok")
         elif 2.94159 <= angle <= 3.14159:
             bin = True
+            print("final bin ok")
         else:
             bin = False
+            print("final bin not ok")
 
         if self.current_bin == False:
             bin = False