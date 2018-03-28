e082012b19f9702218e4f9ada1bdb5a73c732019
sgill2 <samgill@dhcp-204148.bio.uci.edu>
2018-03-27 18:23:01 -0700

    changed to unittest framework

--- blues/tests/test_dart.py
+++ blues/tests/test_dart.py
@@ -9,6 +9,7 @@ import mdtraj as md
 from simtk import unit
 import simtk.openmm as mm
 import numpy as np
+import unittest
 #from simtk.openmm.app import OBC2
 
 class MolEdit(MolDart):
@@ -30,57 +31,64 @@ class MolEdit(MolDart):
         return new_sys, new_int
 
 
+class ReverseDartTester(unittest.TestCase):
+    """
+    Tests that the ic dart move is reversible
+
+    """
+    def setUp(self):
+        #Define some options
+        opt = { 'temperature' : 300.0, 'friction' : 1, 'dt' : 0.0005,
+                'nIter' : 4, 'nstepsNC' : 10, 'nstepsMD' : 10,
+                'nonbondedMethod' : 'CutoffNonPeriodic', 'nonbondedCutoff': 10, 'constraints': 'HBonds',
+                'trajectory_interval' : 1000, 'reporter_interval' : 1000,
+                'platform' : 'CPU',
+                'outfname' : 't4-tol',
+                'nprop':1,
+                'prop_lambda':0.10,
+                'implicitSolvent': None,
+                'verbose' : False,
+                }
+
+        prmtop = utils.get_data_filename('blues', 'tests/data/vacVA.prmtop')
+        inpcrd = utils.get_data_filename('blues', 'tests/data/VAn68.pdb')
+
+        self.struct = parmed.load_file(prmtop, xyz=inpcrd)
+        pdb_files = [ [utils.get_data_filename('blues', 'tests/data/VA68.pdb')], [utils.get_data_filename('blues', 'tests/data/VAn68.pdb')]]
+
+        fit_atoms = [0, 4, 16, 18, 20, 26]
+
+        self.ligand = MolEdit(structure=self.struct, resname=list(range(29)),
+                                          pdb_files=pdb_files,
+                                          fit_atoms=fit_atoms,
+                                          restraints=False,
+                                          restrained_receptor_atoms=[622, 2592, 2425],
+                                          rigid_move=False
+                                          )
+
+        # Initialize object that proposes moves.
+        self.ligand_mover = MoveEngine(self.ligand)
+
+        # Generate the MD, NCMC, ALCHEMICAL Simulation objects
+        self.simulations = SimulationFactory(self.struct, self.ligand_mover, **opt)
+        self.simulations.createSimulationSet()
+
+        self.blues = Simulation(self.simulations, self.ligand_mover, **opt)
+
+    def test_dartreverse(self):
+
+        #get context and set positions to end, see if get same positions as beginning
+        begin_traj = md.load(utils.get_data_filename('blues', 'tests/data/dart_start.pdb' ))
+        end_traj = md.load(utils.get_data_filename('blues', 'tests/data/dart_end.pdb'))
+
+        end_pos = end_traj.openmm_positions(0)
+
+        self.blues.md_sim.context.setPositions(end_pos)
+        begin_compare = self.ligand.move(self.blues.md_sim.context).getState(getPositions=True).getPositions(asNumpy=True)
+        #check that the reverse of the move gives the same positions
+        assert np.allclose(begin_compare._value, begin_traj.openmm_positions(0)._value, rtol=1e-4, atol=1e-4)
 
 
-def test_dartreverse(platform_name):
-    #Define some options
-    opt = { 'temperature' : 300.0, 'friction' : 1, 'dt' : 0.0005,
-            'nIter' : 4, 'nstepsNC' : 10, 'nstepsMD' : 10,
-            'nonbondedMethod' : 'CutoffNonPeriodic', 'nonbondedCutoff': 10, 'constraints': 'HBonds',
-            'trajectory_interval' : 1000, 'reporter_interval' : 1000,
-            'platform' : platform_name,
-            'outfname' : 't4-tol',
-            'nprop':1,
-            'prop_lambda':0.10,
-            'implicitSolvent': None,
-            'verbose' : False,
-            }
+if __name__ == "__main__":
+        unittest.main()
 
-    prmtop = utils.get_data_filename('blues', 'tests/data/vacVA.prmtop')
-    inpcrd = utils.get_data_filename('blues', 'tests/data/VAn68.pdb')
-
-    struct = parmed.load_file(prmtop, xyz=inpcrd)
-    pdb_files = [ [utils.get_data_filename('blues', 'tests/data/VA68.pdb')], [utils.get_data_filename('blues', 'tests/data/VAn68.pdb')]]
-
-    fit_atoms = [0, 4, 16, 18, 20, 26]
-
-    ligand = MolEdit(structure=struct, resname=list(range(29)),
-                                      pdb_files=pdb_files,
-                                      fit_atoms=fit_atoms,
-                                      restraints=False,
-                                      restrained_receptor_atoms=[622, 2592, 2425],
-                                      rigid_move=False
-                                      )
-
-    # Initialize object that proposes moves.
-    ligand_mover = MoveEngine(ligand)
-
-    # Generate the MD, NCMC, ALCHEMICAL Simulation objects
-    simulations = SimulationFactory(struct, ligand_mover, **opt)
-    simulations.createSimulationSet()
-
-    blues = Simulation(simulations, ligand_mover, **opt)
-
-    #get context and set positions to end, see if get same positions as beginning
-    begin_traj = md.load(utils.get_data_filename('blues', 'tests/data/dart_start.pdb' ))
-    end_traj = md.load(utils.get_data_filename('blues', 'tests/data/dart_end.pdb'))
-
-    end_pos = end_traj.openmm_positions(0)
-
-    blues.md_sim.context.setPositions(end_pos)
-    begin_compare = ligand.move(blues.md_sim.context).getState(getPositions=True).getPositions(asNumpy=True)
-    #check that the reverse of the move gives the same positions
-    assert np.allclose(begin_compare._value, begin_traj.openmm_positions(0)._value, rtol=1e-4, atol=1e-4)
-
-
-test_dartreverse('CPU')
