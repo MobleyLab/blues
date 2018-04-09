8d09a7ef25a9455d07eeccfb7618dba81be8d6b4
sgill2 <samgill@dhcp-204148.bio.uci.edu>
2018-04-09 15:29:20 -0700

    updated all system setup to match the correct units

--- blues/tests/test_smartdart.py
+++ blues/tests/test_smartdart.py
@@ -132,7 +132,7 @@ class DartLoaderTester(unittest.TestCase):
                            'lambda_electrostatics' : 'step(0.2-lambda)- 1/0.2*lambda*step(0.2-lambda) + 1/0.2*(lambda-0.8)*step(lambda-0.8)' }
         self.opt = { 'temperature' : 300.0, 'friction' : 1, 'dt' : 0.002,
                 'nIter' : 10, 'nstepsNC' : 10, 'nstepsMD' : 50,
-                'nonbondedMethod' : 'PME', 'nonbondedCutoff': 10, 'constraints': 'HBonds',
+                'nonbondedMethod' : 'PME', 'nonbondedCutoff': 1, 'constraints': 'HBonds',
                 'trajectory_interval' : 10, 'reporter_interval' : 10,
                 'platform' : None, 'outfname' : 'smartdart-test',
                 'verbose' : False }
