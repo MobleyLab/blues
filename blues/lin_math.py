0460d1f694309f29047f0808d431dd299d1338d9
sgill2 <samgill@dhcp-204148.bio.uci.edu>
2018-04-12 13:54:02 -0700

    Cleanup (unused) functions

--- blues/lin_math.py
+++ blues/lin_math.py
@@ -42,42 +42,6 @@ def adjust_angle(a,b, radians, maintain_magnitude=True):
         c = c / np.linalg.norm(c) * mag_a
     return c
 
-
-def adjust_angle1(a,b, radians, maintain_magnitude=True):
-    '''
-    Adjusts angle a so that the angle between a and b
-    matches the specified radians. The change in a
-    occurs in the plane of a and b.
-
-    Arguments
-    ---------
-    a, 1x3 np.array:
-        The vector to be adjusted to change the angle
-    b, 1x3 np.array:
-        The vector to act as a base and creates the
-        angle with a.
-    radians, float:
-        Radian you want to adjust the angle to.
-    maintain_magnitude, boolean:
-        If True adjusts the returned vector keeps
-        the same magnitude as a. If false returns
-        a normalized vector
-    Returns
-    -------
-    c, 1x3 np.array:
-        Vector adjusted so that the angle made
-        from c and b is the value specifed by radians.
-    '''
-    plane = np.cross(a,b) / np.linalg.norm(np.cross(a,b))
-    change = a*np.cos(radians) + (np.cross(plane, a))*np.sin(radians) + plane*(np.dot(plane, a))*(1-np.cos(radians))
-    c = change
-    if maintain_magnitude == True:
-        mag_a = np.linalg.norm(a)
-        c = c / np.linalg.norm(c) * mag_a
-
-    return c
-
-
 def calc_rotation_matrix(vec_ref, vec_target):
     '''calculate the rotation matrix that will rotate vec_ref to vec_target
         Note: will fail if vectors are in exact opposite directions
@@ -105,6 +69,33 @@ def calc_rotation_matrix(vec_ref, vec_target):
     return R
 
 def rigid_transform_3D(A, B, center):
+    """
+    Finds the optimal rotation matrix to overlay
+    A onto B, assuming that the centroid is
+    located at the position of the `center` index.
+
+    Parameters
+    ----------
+    A: 3x3 np.array
+        simulation positions
+    B: 3x3 np.array
+        comparison positions
+    center: int
+        Index of the center atom (either 1,2 or 3)
+    Returns
+    R: 3x3 np.array
+        Rotation matrix to overlay A onto B
+    t: float
+        The distance between the centroid of A and B
+    centroid_A: 1x3 np.array
+        The position of centroid_A
+    centroid_B: 1x3np.array
+        The position of centroid_B
+    centroid_difference: 1x3 np.array
+        Vector corresponding to the difference
+        between the centroid of A and B
+
+    """
     assert len(A) == len(B)
     N = A.shape[0]; # total points
     centroid_A = A[center]
@@ -125,87 +116,25 @@ def rigid_transform_3D(A, B, center):
 
     return R, t, centroid_A, centroid_B, centroid_difference
 
-
-def old_rigid_transform_3D(A, B):
-    assert len(A) == len(B)
-    N = A.shape[0]; # total points
-    centroid_A = np.mean(A, axis=0)
-    centroid_B = np.mean(B, axis=0)
-    # centre the points
-    AA = A - np.tile(centroid_A, (N, 1))
-    BB = B - np.tile(centroid_B, (N, 1))
-    # dot is matrix multiplication for array
-    H = np.dot(np.transpose(AA), BB)
-    U, S, Vt = np.linalg.svd(H)
-    R = np.dot(Vt.T, U.T)
-    # special reflection case
-    if np.linalg.det(R) < 0:
-       Vt[2,:] *= -1
-       R = np.dot(Vt.T, U.T)
-    centroid_difference = centroid_B - centroid_A
-    t = -np.dot(R, centroid_A.T) + centroid_B.T
-
-    return R, t, centroid_A, centroid_B, centroid_difference
-
-def getRotTrans(ares, bres, center):
+def getRotTrans(apos, bpos, center):
     '''
-    Get rotation and translation of rigid pose
+    Get rotation and translation of rigid pose for a three body
+    system.
 
-    Arguments
+    Parameters
     ---------
-    apos: nx3 np.array
+    apos: 3x3 np.array
         simulation positions
-    bpos: nx3 np.array
+    bpos: 3x3 np.array
         comparison positions
-    residueList
+    center:
+        The index of the center atom (either 1, 2 or 3)
+    Returns
+    -------
+    rot: 3x3 np.array
+        Rotation matrix for overlapping a onto b
+    centroid_difference, float
+        Difference between the centroids.
     '''
-    rot, trans, centa, centb, centroid_difference = rigid_transform_3D(ares, bres, center)
+    rot, trans, centa, centb, centroid_difference = rigid_transform_3D(apos, bpos, center)
     return rot, centroid_difference
-
-
-def kabsch(p, q, center):
-    """
-    The optimal rotation matrix U is calculated and then used to rotate matrix
-    P unto matrix Q so the minimum root-mean-square deviation (RMSD) can be
-    calculated.
-    Using the Kabsch algorithm with two sets of paired point P and Q,
-    centered around the center-of-mass.
-    Each vector set is represented as an NxD matrix, where D is the
-    the dimension of the space.
-    The algorithm works in three steps:
-    - a translation of P and Q
-    - the computation of a covariance matrix C
-    - computation of the optimal rotation matrix U
-    http://en.wikipedia.org/wiki/Kabsch_algorithm
-    Parameters:
-    P -- (N, number of points)x(D, dimension) matrix
-    Q -- (N, number of points)x(D, dimension) matrix
-    Returns:
-    U -- Rotation matrix
-    """
-    N = p.shape[0]; # total points
-
-    P = p - np.tile(p[center], (N, 1))
-    Q = q - np.tile(q[center], (N, 1))
-
-    # Computation of the covariance matrix
-    C = np.dot(np.transpose(P), Q)
-
-    # Computation of the optimal rotation matrix
-    # This can be done using singular value decomposition (SVD)
-    # Getting the sign of the det(V)*(W) to decide
-    # whether we need to correct our rotation matrix to ensure a
-    # right-handed coordinate system.
-    # And finally calculating the optimal rotation matrix U
-    # see http://en.wikipedia.org/wiki/Kabsch_algorithm
-    V, S, W = np.linalg.svd(C)
-    d = (np.linalg.det(V) * np.linalg.det(W)) < 0.0
-
-    if d:
-        S[-1] = -S[-1]
-        V[:, -1] = -V[:, -1]
-
-    # Create Rotation matrix U
-    U = np.dot(V, W)
-
-    return U
