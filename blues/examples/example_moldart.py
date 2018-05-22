7bc1e20780e12d2e47ff1f0e61ceb2f49d781763
sgill2 <samgill@dhcp-204148.bio.uci.edu>
2018-05-22 11:45:14 -0700

    added rigid_ring option as default

--- blues/examples/example_moldart.py
+++ blues/examples/example_moldart.py
@@ -49,6 +49,7 @@ def runNCMC(platform_name):
                                       pdb_files=[posA, posB],
                                       fit_atoms=fit_atoms,
                                       restrained_receptor_atoms=[1605, 1735, 1837],
+                                      rigid_ring=True
                                       )
 
     # Initialize object that proposes moves.
