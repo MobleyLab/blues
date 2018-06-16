c3d7e5226fac46ff42abccb79eacc2663a1b464e
Nathan M. Lim <nathanmlim@gmail.com>
2018-06-16 12:55:45 -0700

    Restore install in build command.

--- devtools/conda-recipe/conda_upload.sh
+++ devtools/conda-recipe/conda_upload.sh
@@ -1,9 +1,15 @@
-# Only need to change these two variables
-PKG_NAME=blues
-USER=nathanmlim
+#!/bin/bash
+set -e
 
-OS=$TRAVIS_OS_NAME-64
-conda config --set anaconda_upload no
-export CONDA_BLD_FILE="$CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-py${CONDA_PY}_${PKG_BUILDNUM}.tar.bz2"
-conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l dev $CONDA_BLD_FILE --force
+echo "Converting conda package..."
+conda convert --platform all $CONDA_BLD_PATH/linux-64/$PKG_NAME-*.tar.bz2 --output-dir conda-bld/
+
+echo "Deploying to Anaconda.org..."
+anaconda -t $CONDA_UPLOAD_TOKEN $CONDA_BLD_PATH/**/$PKG_NAME-*.tar.bz2
+
+echo "Successfully deployed to Anaconda.org."
+exit 0
+
+#export CONDA_BLD_FILE="$CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-py${CONDA_PY}_${PKG_BUILDNUM}.tar.bz2"
+#conda build .
+#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l dev $CONDA_BLD_FILE --force
