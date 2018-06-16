67c4c6a1c92b1a2fd4c09f149e4589a56a15881e
Nathan Lim <nathanmlim@gmail.com>
2018-06-15 17:54:40 -0700

    More fixes for bld file name

--- devtools/conda-recipe/conda_upload.sh
+++ devtools/conda-recipe/conda_upload.sh
@@ -4,6 +4,6 @@ USER=nathanmlim
 
 OS=$TRAVIS_OS_NAME-64
 conda config --set anaconda_upload no
-export CONDA_BLD_FILE="$CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-`date +%Y.%m.%d`.tar.bz2"
+export CONDA_BLD_FILE="$CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION.tar.bz2"
 conda build . --output $CONDA_BLD_FILE
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_FILE --force
