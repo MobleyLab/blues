cfabb6eea4d7cdf9db562273a2f5dc985d28cd7e
Nathan Lim <nathanmlim@gmail.com>
2018-06-15 17:24:17 -0700

    More fixes for conda build file

--- devtools/conda-recipe/conda_upload.sh
+++ devtools/conda-recipe/conda_upload.sh
@@ -3,7 +3,6 @@ PKG_NAME=blues
 USER=nathanmlim
 
 OS=$TRAVIS_OS_NAME-64
-mkdir ~/conda-bld
 conda config --set anaconda_upload no
-conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-`date +%Y.%m.%d`.tar.bz2 --force
+conda build . --output $CONDA_BLD_FILE
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_FILE --force
