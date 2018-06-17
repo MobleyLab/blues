c7d26641ca5a8b38d31fa74fac13ad99015c2012
Nathan M. Lim <nathanmlim@gmail.com>
2018-06-16 19:46:41 -0700

    Hardcore path for conda package

--- devtools/conda-recipe/conda_upload.sh
+++ devtools/conda-recipe/conda_upload.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-
+export CONDA_BLD_FILE="$CONDA_BLD_PATH/noarch/$PKG_NAME-0.2.3-py${CONDA_PY}_${PKG_BUILDNUM}.tar.bz2"
 echo "Converting conda package..."
-conda convert --platform all $CONDA_BLD_PATH/linux-64/$PKG_NAME-*.tar.bz2 --output-dir conda-bld/
+conda convert --platform all ${CONDA_BLD_FILE} --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l dev $CONDA_BLD_PATH/**/$PKG_NAME-*.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l dev ${CONDA_BLD_FILE} --force
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
