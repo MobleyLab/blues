769904b34f6b7f3838fe4361a76a3368e204781a
Nathan M. Lim <nathanmlim@gmail.com>
2018-07-22 18:32:40 -0700

    Changes for release 0.2.2

--- devtools/conda-recipe/conda_upload.sh
+++ devtools/conda-recipe/conda_upload.sh
@@ -6,7 +6,11 @@ CONDA_BLD_FILE=${HOME}/miniconda/conda-bld/noarch/blues-*.tar.bz2
 #conda convert --platform all ${CONDA_BLD_FILE} --output-dir conda-bld/
 
 echo "Deploying $CONDA_BLD_FILE to Anaconda.org..."
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USERNAME -l dev ${CONDA_BLD_FILE} --force
+### DEV BUILDS
+#anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USERNAME -l dev ${CONDA_BLD_FILE} --force
+
+### RELEASE BUILDS
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USERNAME -l main ${CONDA_BLD_FILE} --force
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
