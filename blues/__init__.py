4dd74f483271ee97adc1bb8055a952796af0dc7b
Nathan M. Lim <nathanmlim@gmail.com>
2018-08-25 22:37:27 -0700

    Add configuration files for versioneer and .github templates

--- blues/__init__.py
+++ blues/__init__.py
@@ -3,6 +3,17 @@
 BLUES
 """
 # Define global version.
-from . import version
-__version__ = version.version
+#from . import version
+#__version__ = version.version
+
 from blues import moves, ncmc_switching, simulation, utils, reporters
+
+# Add imports here
+from .blues import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
