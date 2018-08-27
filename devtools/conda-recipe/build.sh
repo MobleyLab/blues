39d4f0320627b9a4f6407bd34971d18dd6d24010
Nathan M. Lim <nathanmlim@gmail.com>
2018-08-27 13:14:12 -0700

    More changes to try to fix travis

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -2,4 +2,6 @@
 
 # Build the python package, don't let setuptools/pip try to get packages
 # $PYTHON setup.py develop --no-deps
-pip install . --no-deps
+#pip install . --no-deps
+#$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
