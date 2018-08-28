319fc72ee6908e287c1d95a84cfa704c3300b190
Nathan M. Lim <limn1@uci.edu>
2018-08-28 15:01:58 -0700

    Restore NCMC/MD syncing behavior (#150)
    * Properly reset simulations after NCMC and before MD.
    * Add check in potential energy when move is rejected.
    * Remove code redundancies in updating stateTable. 
    * Add check when freezing atoms.

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ DOCLINES = __doc__.split("\n")
 
 ########################
 VERSION = "0.2.2"  # Primary base version of the build
-DEVBUILD = "0"      # Dev build status, Either None or Integer as string
-ISRELEASED = True  # Are we releasing this as a full cut?
+DEVBUILD = "1"      # Dev build status, Either None or Integer as string
+ISRELEASED = False  # Are we releasing this as a full cut?
 __version__ = VERSION
 ########################
 
