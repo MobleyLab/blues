12d07977d96b6bd7c86c67d566118dacf0f35bf6
Nathan Lim <nathanmlim@gmail.com>
2019-05-04 12:35:53 -0700

    Increase number of iterations in ethylene test

--- blues/tests/test_ethylene.py
+++ blues/tests/test_ethylene.py
@@ -35,7 +35,7 @@ def runEthyleneTest(N):
         'dt': 1 * unit.femtoseconds,
         'friction': 1 / unit.picoseconds,
         'temperature': 200 * unit.kelvin,
-        'nIter': 250,
+        'nIter': 500,
         'nstepsMD': 20,
         'nstepsNC': 20,
         'propSteps': 20,
